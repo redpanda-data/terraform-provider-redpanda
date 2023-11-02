5ae912d62f01fc6e8620c3fb0b97dc6dcc8f11a6
gene-redpanda <123959009+gene-redpanda@users.noreply.github.com>
2023-11-02 15:40:42 -0400

    client generated

---
+++ generate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+go install github.com/bufbuild/buf/cmd/buf@latest
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+rm -rf proto/gen
+buf generate buf.build/redpandadata/cloud
+buf generate buf.build/redpandadata/common
