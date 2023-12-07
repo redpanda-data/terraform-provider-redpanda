0ae138499df02896b2a1848c168d32c12106648b
Johannes Brüderl <johannes@redpanda.fritz.box>
2023-12-07 09:20:44 +0100

    proto: add dataplane api generated code

--- generate.sh
+++ generate.sh
@@ -4,4 +4,5 @@ go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 rm -rf proto/gen
 buf generate buf.build/redpandadata/cloud
+buf generate buf.build/redpandadata/dataplane
 buf generate buf.build/redpandadata/common
