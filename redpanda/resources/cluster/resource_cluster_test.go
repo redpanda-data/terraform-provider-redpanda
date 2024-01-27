75ad2dba94b16de926236b99e846102043244fba
Rogger Vasquez <rvasque3@gmail.com>
2024-01-26 16:48:17 -0800

    [chore] remove TODOs and add cluster spec validators
    This removes TODOs that are now issues in GH and
    also add error checks when either the cluster type
    or cloud provider are invalid.

--- redpanda/resources/cluster/resource_cluster_test.go
+++ redpanda/resources/cluster/resource_cluster_test.go
@@ -52,7 +52,7 @@ func TestGenerateClusterRequest(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := GenerateClusterRequest(tt.args.model); !reflect.DeepEqual(got, tt.want) {
+			if got, _ := GenerateClusterRequest(tt.args.model); !reflect.DeepEqual(got, tt.want) {
 				fmt.Println("got")
 				spew.Dump(got)
 				fmt.Println("want")
