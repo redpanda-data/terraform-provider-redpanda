97b5d4fc24153f429432dbd689427d5b43d0a02c
Rogger Vasquez <rvasque3@gmail.com>
2024-02-28 11:32:29 -0800

    [chore] Remove unused attributes of Redpanda provider
    These should be supported later, but we should
    remove them from now since the auto-generated
    docs will pick them up.

--- redpanda/models/redpanda.go
+++ redpanda/models/redpanda.go
@@ -21,9 +21,6 @@ import (
 
 // Redpanda represents the Terraform schema for the Redpanda TF provider.
 type Redpanda struct {
-	ClientID      types.String   `tfsdk:"client_id"`
-	ClientSecret  types.String   `tfsdk:"client_secret"`
-	CloudProvider types.String   `tfsdk:"cloud_provider"`
-	Region        types.String   `tfsdk:"region"`
-	Zones         []types.String `tfsdk:"zones"`
+	ClientID     types.String `tfsdk:"client_id"`
+	ClientSecret types.String `tfsdk:"client_secret"`
 }
