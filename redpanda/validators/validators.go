1123a06dee78aa1f7e17dab8ffc52fbe50596876
gene-redpanda <123959009+gene-redpanda@users.noreply.github.com>
2024-07-31 19:28:59 -0500

    docs: change comment on validators

--- redpanda/validators/validators.go
+++ redpanda/validators/validators.go
@@ -13,7 +13,7 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 
-// Package validators contains generally useful validation functions for the Redpanda provider.
+// Package validators contains general purpose validation functions for the Redpanda provider.
 package validators
 
 import (
