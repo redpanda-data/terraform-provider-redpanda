428068d604a45a1fad2cdba1c20c9ddef17e1a2a
gene-redpanda <123959009+gene-redpanda@users.noreply.github.com>
2024-08-05 14:02:48 -0500

    feat: examples rework
    Goals were to consolidate examples into more general purpose cluster creation, expand the amount of stuff each one does so that users have to go to fewer places and have them in standardized locations.
    
    This is a NON FUNCTIONAL COMMIT, acceptance testing will fail.

--- examples/cluster/gcp/main.tf
+++ examples/cluster/gcp/main.tf
@@ -27,6 +27,15 @@ resource "redpanda_cluster" "test" {
   tags = {
     "key" = "value"
   }
+  gcp_private_service_connect = {
+    enabled               = true
+    global_access_enabled = true
+    consumer_accept_list = [
+      {
+        source = "projects/123456789012"
+      }
+    ]
+  }
 }
 
 variable "cluster_name" {
@@ -55,4 +64,57 @@ variable "cloud_provider" {
 
 variable "throughput_tier" {
   default = "tier-1-gcp-um4g"
+}
+
+
+resource "redpanda_user" "test" {
+  name            = var.user_name
+  password        = var.user_pw
+  mechanism       = var.mechanism
+  cluster_api_url = redpanda_cluster.test.cluster_api_url
+}
+
+resource "redpanda_topic" "test" {
+  name               = var.topic_name
+  partition_count    = var.partition_count
+  replication_factor = var.replication_factor
+  cluster_api_url    = redpanda_cluster.test.cluster_api_url
+  allow_deletion     = true
+}
+
+
+resource "redpanda_acl" "test" {
+  resource_type         = "TOPIC"
+  resource_name         = redpanda_topic.test.name
+  resource_pattern_type = "LITERAL"
+  principal             = "User:${redpanda_user.test.name}"
+  host                  = "*"
+  operation             = "READ"
+  permission_type       = "ALLOW"
+  cluster_api_url       = redpanda_cluster.test.cluster_api_url
+}
+
+
+variable "user_name" {
+  default = "test-username"
+}
+
+variable "user_pw" {
+  default = "password"
+}
+
+variable "mechanism" {
+  default = "scram-sha-256"
+}
+
+variable "topic_name" {
+  default = "test-topic"
+}
+
+variable "partition_count" {
+  default = 3
+}
+
+variable "replication_factor" {
+  default = 3
 }
