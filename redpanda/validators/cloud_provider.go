778a0432fcf4ec9317d3445a5d7c7fab1a78a3a4
gene-redpanda <123959009+gene-redpanda@users.noreply.github.com>
2024-08-05 14:04:06 -0500

    feat: add cloudprovider validation for links
    The azure_private_link, aws_private_link and gcp_private_service_connect fields can only be used with their respective provider. This was causing fails on the cluster create call -- IOW about 20m into the process. Not ideal! The validator fails things at the terraform plan stage which saves the end user an immense amount of time.

---
+++ redpanda/validators/cloud_provider.go
@@ -0,0 +1,46 @@
+package validators
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+// CloudProviderDependentValidator is a custom validator to ensure that an attribute is only set when cloud_provider is a specific value
+// For example when using this on aws_private_link it will ensure that the HCL fails validation unless cloud_provider is set to "aws"
+// AttributeName should be the name of the attribute that is being validated
+// CloudProvider should be the value of cloud_provider that the attribute is dependent on
+type CloudProviderDependentValidator struct {
+	AttributeName string
+	CloudProvider string
+}
+
+// Description provides a description of the validator
+func (v CloudProviderDependentValidator) Description(ctx context.Context) string {
+	return fmt.Sprintf("ensures that %s is only set when cloud_provider is %s", v.AttributeName, v.CloudProvider)
+}
+
+// MarkdownDescription provides a description of the validator in markdown format
+func (v CloudProviderDependentValidator) MarkdownDescription(ctx context.Context) string {
+	return fmt.Sprintf("Ensures that `%s` is only set when `cloud_provider` is `%s`", v.AttributeName, v.CloudProvider)
+}
+
+func (v CloudProviderDependentValidator) ValidateObject(ctx context.Context, req validator.ObjectRequest, resp *validator.ObjectResponse) {
+	var cloudProvider types.String
+	if diags := req.Config.GetAttribute(ctx, req.Path.ParentPath().AtName("cloud_provider"), &cloudProvider); diags.HasError() {
+		resp.Diagnostics.Append(diags...)
+		return
+	}
+
+	// If the object is set and cloud_provider is known but doesn't match, add an error
+	if !req.ConfigValue.IsNull() && !cloudProvider.IsUnknown() && cloudProvider.ValueString() != v.CloudProvider {
+		resp.Diagnostics.AddAttributeError(
+			req.Path,
+			"Invalid Configuration",
+			fmt.Sprintf("%s can only be set when cloud_provider is %s, but it is set to %s",
+				v.AttributeName, v.CloudProvider, cloudProvider.ValueString()),
+		)
+	}
+}
