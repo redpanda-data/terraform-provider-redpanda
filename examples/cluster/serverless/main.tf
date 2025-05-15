a06021b83b7629351fbc7a93bf5f4fabcc766b59
gene-redpanda <123959009+gene-redpanda@users.noreply.github.com>
2025-05-15 17:20:37 -0500

    ci: serverless example improvement
    Add topic, user and acl creation to serverless

--- examples/cluster/serverless/main.tf
+++ examples/cluster/serverless/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    redpanda = {
+      source  = "redpanda-data/redpanda"
+      version = "0.10.0"
+    }
+  }
+}
 provider "redpanda" {
 }
 resource "redpanda_resource_group" "test" {
@@ -20,4 +28,56 @@ variable "cluster_name" {
 
 variable "region" {
   default = "eu-west-1"
+}
+
+resource "redpanda_user" "test" {
+  name            = var.user_name
+  password        = var.user_pw
+  mechanism       = var.mechanism
+  cluster_api_url = redpanda_serverless_cluster.test.cluster_api_url
+}
+
+resource "redpanda_topic" "test" {
+  name               = var.topic_name
+  partition_count    = var.partition_count
+  replication_factor = var.replication_factor
+  cluster_api_url    = redpanda_serverless_cluster.test.cluster_api_url
+  allow_deletion     = true
+}
+
+
+resource "redpanda_acl" "test" {
+  resource_type         = "TOPIC"
+  resource_name         = redpanda_topic.test.name
+  resource_pattern_type = "LITERAL"
+  principal             = "User:${redpanda_user.test.name}"
+  host                  = "*"
+  operation             = "READ"
+  permission_type       = "ALLOW"
+  cluster_api_url       = redpanda_serverless_cluster.test.cluster_api_url
+}
+
+
+variable "user_name" {
+  default = "test-username"
+}
+
+variable "user_pw" {
+  default = "password"
+}
+
+variable "mechanism" {
+  default = "scram-sha-256"
+}
+
+variable "topic_name" {
+  default = "test-topic"
+}
+
+variable "partition_count" {
+  default = 3
+}
+
+variable "replication_factor" {
+  default = 3
 }
