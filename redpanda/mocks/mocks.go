3e6a0a45aec08e3e2f636877c26c9c9095276f4b
gene-redpanda <123959009+gene-redpanda@users.noreply.github.com>
2025-11-20 05:56:41 -0600

    chore: mock gen

--- redpanda/mocks/mocks.go
+++ redpanda/mocks/mocks.go
@@ -27,3 +27,4 @@ package mocks
 //go:generate mockgen -destination=./mock_throughput_service_client.go -package=mocks buf.build/gen/go/redpandadata/cloud/grpc/go/redpanda/api/controlplane/v1beta2/controlplanev1beta2grpc ThroughputTierServiceClient
 //go:generate mockgen -destination=./mock_schema_registry_acl_client.go -package=mocks github.com/redpanda-data/terraform-provider-redpanda/redpanda/kclients SchemaRegistryACLClientInterface
 //go:generate mockgen -destination=./mock_schema_registry_client.go -package=mocks github.com/redpanda-data/terraform-provider-redpanda/redpanda/resources/schema SRClienter
+//go:generate mockgen -destination=./mock_security_service_client.go -package=mocks buf.build/gen/go/redpandadata/dataplane/grpc/go/redpanda/api/console/v1alpha1/consolev1alpha1grpc SecurityServiceClient
